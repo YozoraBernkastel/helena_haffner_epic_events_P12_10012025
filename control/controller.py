0999a5fe069ebb7409db096a2e8a73943c62db11
Featherine <helaena.haffner@gmail.com>
2025-01-30 12:11:12 +0100

    #04 password salted and hashed

--- control/controller.py
+++ control/controller.py
@@ -1,3 +1,4 @@
+import bcrypt
 from models.db_models import Collaborator, Client, Contract, Event
 from view.view import View
 
@@ -16,6 +17,12 @@ class Controller:
         Contract.create_table()
         Event.create_table()
 
+    @staticmethod
+    def dress_password(password: str) -> bytes:
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password=bytes(password, encoding="ascii"), salt=salt)
+        return hashed
+
     def display_welcome_menu(self):
 
         while self.user is None:
@@ -25,6 +32,8 @@ class Controller:
                 print("Utilisateur ou mot de passe inconnu.")
 
         # todo générer JWToken ici !! Possibilité de se déconnecter ? Comment ?
+        memorize: bool = View.remember_me()
         print(f"{self.user.username = }")
+        print(f"{memorize = }")
 
         # todo penser à sauvegarder l'id de l'utilisateur par exemple.
